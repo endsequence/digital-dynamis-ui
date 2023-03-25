b3483e40001cc305e9eb3f2c35c04e0c1bf56f92
Umesh Mahato <umesh.mahato@publicissapient.com>
2023-03-25 18:52:22 +0400

    added highchart

--- src/views/Manage.js
+++ src/views/Manage.js
@@ -12,6 +12,8 @@ import TitleBar from "../components/TitleBar";
 import DiscoverCard from "../components/DiscoverCard";
 import Copyright from "../components/Copyright";
 import axios from "axios";
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
 const theme = createTheme();
 export default function Manage() {
@@ -57,6 +59,21 @@ export default function Manage() {
           <Container maxWidth="lg">
             <Grid container spacing={4} sx={{ my: 2 }}>
               <Grid item xs={12} sm={6} md={6} lg={6}>
+
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={{
+                    title: {
+                      text: 'My chart'
+                    },
+                    series: [{
+                      data: [1, 2, 3]
+                    }]
+                  }}
+                />
+
+
+
                 <Autocomplete
                   sx={{ width: "100%" }}
                   id="free-solo-demo"
